5ad3b4867408e8d48594d2479af887fe0717fda5
Femi Ayodeji <femidotayodeji@gmail.com>
2020-12-31 05:04:11 +0100

    Added transition to enter highscore and start states

--- src/states/GameOverState.lua
+++ src/states/GameOverState.lua
@@ -2,11 +2,33 @@ GameOverState = Class{__includes = BaseState}
 
 function GameOverState:enter(params)
     self.score = params.score
+    self.highScores = params.highscores
 end
 
 function GameOverState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then 
-        gStateMachine:change('start')
+        local highScore = false
+        local scoreIndex = 11
+
+        for i = 10, 1, -1 do 
+            local score = self.highScores[i].score or 0
+            if self.score > score then 
+                highScore = true
+                highScoreIndex = i
+            end
+        end
+
+        if highScore then gSounds['high-score']:play()
+            gStateMachine:change('enter-high-score', {
+                highScores = self.highScores,
+                score = self.score,
+                scoreIndex = highScoreIndex
+            })
+        else
+            gStateMachine:change('start', {
+                highscores = self.highscores
+            })
+        end
     end
 
     if love.keyboard.wasPressed('escape') then 
@@ -16,23 +38,8 @@ end
 
 function GameOverState:render()
     love.graphics.setFont(gFonts['large'])
-    love.graphics.printf(
-        'GAME OVER',
-        0, 
-        VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 
-        'center' 
-    )
+    love.graphics.printf('GAME OVER', 0, VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 'center')
     love.graphics.setFont(gFonts['medium'])
-    love.graphics.printf(
-        'Final Score: ' .. tostring(self.score),
-        0, 
-        VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 
-        'center'
-    )
-    love.graphics.printf(
-        'Press Enter!', 
-        0, 
-        VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 4, VIRTUAL_WIDTH, 
-        'center'
-    )
+    love.graphics.printf( 'Final Score: ' .. tostring(self.score), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter!', 0, VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 4, VIRTUAL_WIDTH, 'center')
 end
